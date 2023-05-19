5f1a09fd10b4493379f8f2460eda0dbcc277ea7f
Dhananjay Goyal <113744303+DhananjayGoyalGL@users.noreply.github.com>
2023-05-19 20:55:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //Information about server and public key
 const api = {
-  key: "7e3f21edee540e6110af347b55eb1ab2",
+  key: "enter the key generated here",
   base: "https://api.openweathermap.org/data/2.5/",
 };
 
